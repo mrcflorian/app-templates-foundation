5c0815049cfbef6ee37c2a157a7037defeb535e0
Florian Marcu <florian.mrc@gmail.com>
2017-05-09 21:59:37 -0700

    AdMob support

---
+++ AppTemplatesFoundation/AppTemplatesFoundation/AppTemplatesCore/Models/ATCGoogleAd.swift
@@ -0,0 +1,25 @@
+//
+//  ATCGoogleAd.swift
+//  WordpressApp
+//
+//  Created by Florian Marcu on 5/9/17.
+//  Copyright © 2017 iOS App Templates. All rights reserved.
+//
+
+import GoogleMobileAds
+import ObjectMapper
+import UIKit
+
+class ATCGoogleAd: ATCBaseModel {
+
+    let googleAdRequest = GADRequest()
+
+    init() {
+    }
+
+    public required init?(map: Map) {
+    }
+
+    public func mapping(map: Map) {
+    }
+}
--- AppTemplatesFoundation/AppTemplatesFoundation/AppTemplatesCore/ViewControllers/ATCCollectionViewController.swift
+++ AppTemplatesFoundation/AppTemplatesFoundation/AppTemplatesCore/ViewControllers/ATCCollectionViewController.swift
@@ -30,17 +30,36 @@ open class ATCCollectionViewController<T: ATCBaseModel & NSCoding & Equatable>: 
         return streamManager
         }()
 
-    var streamObjects: [T] {
+    var streamObjects: [ATCBaseModel] {
         get {
             if let objects = self.streamManager?.streamObjects, objects.count > 0 {
-                return objects
+                return self.streamObjectsWithGoogleAds(streamObjects: objects)
             }
             guard let objectsOnDisk = self.streamObjectsOnDisk else { return [] }
             return objectsOnDisk
         }
     }
 
+    private var googleAds: [ATCGoogleAd] = []
+    private var adUnitID: String?
+    private var shouldInjectGoogleAds: Bool {
+        return adUnitID != nil
+    }
+
     lazy var refreshControl = UIRefreshControl()
+
+    public init(adUnitID: String? = nil) {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        self.adUnitID = adUnitID
+    }
+
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override open func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +72,9 @@ open class ATCCollectionViewController<T: ATCBaseModel & NSCoding & Equatable>: 
         let cellNib = UINib(nibName: "ATCLoadMoreCollectionViewCell", bundle: nil)
         collectionView?.register(cellNib, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "ATCLoadMoreCollectionViewCell")
 
+        let adCellNib = UINib(nibName: "ATCGADBannerCollectionViewCell", bundle: nil)
+        collectionView?.register(adCellNib, forCellWithReuseIdentifier: "ATCGADBannerCollectionViewCell")
+
         if let collectionView = collectionView {
             let collectionViewLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout
             collectionViewLayout?.footerReferenceSize = CGSize(width: collectionView.bounds.width, height: 100)
@@ -61,7 +83,7 @@ open class ATCCollectionViewController<T: ATCBaseModel & NSCoding & Equatable>: 
         loadMoreData()
     }
 
-    public func object(at indexPath: IndexPath) -> T? {
+    public func object(at indexPath: IndexPath) -> ATCBaseModel? {
         if (indexPath.row < streamObjects.count) {
             return streamObjects[indexPath.row]
         }
@@ -76,6 +98,16 @@ open class ATCCollectionViewController<T: ATCBaseModel & NSCoding & Equatable>: 
 
     override open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return streamObjects.count
+    }
+
+    override open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        // Google Ad Cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ATCGADBannerCollectionViewCell", for: indexPath) as! ATCGADBannerCollectionViewCell
+        if let object = self.object(at: indexPath) as? ATCGoogleAd {
+            cell.configure(googleAd: object, adUnitID: self.adUnitID!, viewController: self)
+        }
+        return cell
     }
 
     override open func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -149,9 +181,11 @@ open class ATCCollectionViewController<T: ATCBaseModel & NSCoding & Equatable>: 
     }
 
     private func saveCurrentObjectsToDisk() {
-        let success = NSKeyedArchiver.archiveRootObject(self.streamObjects, toFile: archiveItemsURL.path)
-        if (!success) {
-            print("Unable to write to disk")
+        if let streamObjects = self.streamManager?.streamObjects {
+            let success = NSKeyedArchiver.archiveRootObject(streamObjects, toFile: archiveItemsURL.path)
+            if (!success) {
+                print("Unable to write to disk")
+            }
         }
     }
 
@@ -165,5 +199,24 @@ open class ATCCollectionViewController<T: ATCBaseModel & NSCoding & Equatable>: 
             }
         }
         return true
+    }
+
+    private func streamObjectsWithGoogleAds(streamObjects: [T]) -> [ATCBaseModel] {
+        if self.shouldInjectGoogleAds == false {
+            return streamObjects
+        }
+        var res: [ATCBaseModel] = []
+        var googleAdIndex: Int = 0
+        for (index, object) in streamObjects.enumerated() {
+            res.append(object)
+            if index % 3 == 1 {
+                if googleAdIndex >= googleAds.count {
+                    googleAds.append(ATCGoogleAd())
+                }
+                res.append(googleAds[googleAdIndex])
+                googleAdIndex += 1
+            }
+        }
+        return res
     }
 }
